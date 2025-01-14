026b9acb7f65638ed2c29da8f65e9f03548eabfa
Edward Tanguay (windows) <edward@tanguay.info>
2025-01-14 12:00:54 +0100

    changed name

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,5 +1,15 @@
 export const PageAbout = () => {
 	return (
-		<p>This is the about page.</p>
-	)
-}
+		<p>
+			This site shows examples of using{" "}
+			<a
+				href="https://easy-peasy.dev"
+				target="_blank"
+				className="underline"
+			>
+				easy-peasy Redux
+			</a>
+			.
+		</p>
+	);
+};
